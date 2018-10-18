3bc0edb4bc021b4a88fdbe8c6c9c81c56176f110
Basvanderhoeven <basvdhoevenn@gmail.com>
2018-10-18 10:58:13 +0200

    auth0 redirect fix

--- app/angular/src/app/auth.guard.ts
+++ app/angular/src/app/auth.guard.ts
@@ -16,7 +16,7 @@ export class AuthGuard implements CanActivate {
   canActivate(
     next: ActivatedRouteSnapshot,
     state: RouterStateSnapshot): Observable<boolean> | Promise<boolean> | boolean {
-    if (!this.authService.isAuthenticated) {
+    if (!this.authService.isAuthenticated()) {
       this.router.navigate(['/']);
       return false;
     }

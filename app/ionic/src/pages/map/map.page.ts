1e39da8bcea96f5847309185fee294b47f3a1929
Unknown <spallierkoen@hotmail.com>
2018-11-28 23:34:56 +0100

    added distance calculations

--- app/ionic/src/pages/map/map.page.ts
+++ app/ionic/src/pages/map/map.page.ts
@@ -55,6 +55,7 @@ export class MapPage {
                         title: loc.locationName, 
                         icon: "../../assets/icon/newMarker.png"
                     });
+                    console.log(this.calcDistance(this.playerPos.latitude, marker.latitude, this.playerPos.longitude, marker.longitude)); 
                 }
                 
             } catch{
@@ -101,6 +102,22 @@ export class MapPage {
         }, (err) => {
             console.log(err);
         });
+    }
+    calcDistance(lat1: any, lat2: any, lon1: any, lon2: any){
+        var R = 6371e3; // metres
+        var φ1 = lat1.toRadians();
+        var φ2 = lat2.toRadians();
+        var Δφ = (lat2-lat1) * ((Math.PI)/180); 
+
+        var Δλ = (lon2-lon1) * ((Math.PI)/180);
+
+        var a = Math.sin(Δφ/2) * Math.sin(Δφ/2) +
+                Math.cos(φ1) * Math.cos(φ2) *
+                Math.sin(Δλ/2) * Math.sin(Δλ/2);
+        var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
+
+        var d = R * c;
+        return d; 
     }
     updatePlayerMarker() {
         let watchOptions = {

ccdd409c2fa2814935e2826166256d9db676c79c
Guzse <MyNameIsGuzse@gmail.com>
2019-01-21 16:22:27 +0100

    Final Product Changes

--- app/ionic/src/providers/AlloutAPI/AlloutAPI.ts
+++ app/ionic/src/providers/AlloutAPI/AlloutAPI.ts
@@ -10,7 +10,7 @@ export class AlloutProvider {
     teamName:string;
 
     constructor(private _http: HttpClient) {
-        this.url = "http://localhost:33324/api/v1/"
+        this.url = "https://alloutapi20190121022729.azurewebsites.net/api/v1/"
     };
 
     postTeam(team: Team) {
